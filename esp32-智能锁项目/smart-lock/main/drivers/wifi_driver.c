825ccfbf078575a67d93f38218304df10c94b7cc
eyclst <152983528+eyclst@users.noreply.github.com>
2024-04-17 14:17:13 +0800

    更新 wifi_driver.c

--- esp32-智能锁项目/smart-lock/main/drivers/wifi_driver.c
+++ esp32-智能锁项目/smart-lock/main/drivers/wifi_driver.c
@@ -6,7 +6,7 @@ const char *WIFI_TAG = "wifi station";
 int s_retry_num = 0;
 
 void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data)
+                          int32_ event_id, void *event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
